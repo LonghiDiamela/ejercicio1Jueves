874668140e10ae8f61fb0b23c85ee6b1d41c3462
LonghiDiamela <110548653+LonghiDiamela@users.noreply.github.com>
2022-08-08 17:16:18 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
+
+
+
+console.log("receta de torta");
+console.log("harina, huevos, leche, manteca  azucar");
+console.log("incorporar uno a uno los ingredientes");
+console.log("enmantecar el molde");
+console.log("cocinar en horno a fuego lento durante 45 minutos");
+console.log("dejar enfriar y desmoldar");
+console.log("decorar con dulce de leche y coco");
